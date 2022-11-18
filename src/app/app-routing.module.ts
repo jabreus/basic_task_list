c3e7877e031caae2524e91003e14e1e73d2e1002
Javier <jastincer1998@gmail.com>
2022-11-18 16:35:14 -0500

    adding add-task component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddTaskComponent } from './components/add-task/add-task.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '' , component: AddTaskComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NzInputModule } from 'ng-zorro-antd/input';
+
+import { IconDefinition } from '@ant-design/icons-angular';
+import { PlusOutline, UserOutline, ArrowsAltOutline } from '@ant-design/icons-angular/icons';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
+import { AddTaskComponent } from './components/add-task/add-task.component';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { FormsModule } from '@angular/forms';
+
+const icons: IconDefinition[] = [PlusOutline, UserOutline, ArrowsAltOutline];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTaskComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NzInputModule,
+    NzAvatarModule,
+    NzButtonModule,
+    FormsModule,
+    NzIconModule.forRoot(icons),
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
