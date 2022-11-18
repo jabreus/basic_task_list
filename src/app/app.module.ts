4372a82ac7e489930cadc0e24eb2be1348df598f
Javier <jastincer1998@gmail.com>
2022-11-18 18:04:13 -0500

    adding new buttons and some styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,15 @@ import { AppComponent } from './app.component';
 import { NzInputModule } from 'ng-zorro-antd/input';
 
 import { IconDefinition } from '@ant-design/icons-angular';
-import { PlusOutline, UserOutline, ArrowsAltOutline } from '@ant-design/icons-angular/icons';
+import { PlusSquareOutline, UserOutline, ArrowsAltOutline, CalendarOutline, UnlockOutline, HighlightOutline, QuestionOutline} from '@ant-design/icons-angular/icons';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzAvatarModule } from 'ng-zorro-antd/avatar';
 import { AddTaskComponent } from './components/add-task/add-task.component';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { FormsModule } from '@angular/forms';
+import { NzGridModule } from 'ng-zorro-antd/grid';
 
-const icons: IconDefinition[] = [PlusOutline, UserOutline, ArrowsAltOutline];
+const icons: IconDefinition[] = [PlusSquareOutline, UserOutline, ArrowsAltOutline, CalendarOutline, UnlockOutline, HighlightOutline, QuestionOutline];
 
 @NgModule({
   declarations: [
@@ -28,6 +29,7 @@ const icons: IconDefinition[] = [PlusOutline, UserOutline, ArrowsAltOutline];
     NzAvatarModule,
     NzButtonModule,
     FormsModule,
+    NzGridModule,
     NzIconModule.forRoot(icons),
   ],
   providers: [],
